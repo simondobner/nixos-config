da7f04585e6449cfc8454bf6fe13f585c65e13ef
lbodor <lazar.bodor@ga.gov.au>
2015-12-16 09:46:37 +0000

    Add file command

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+     file
      wget
      git
      vim
