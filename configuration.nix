932ee88eeeccb337146521d834e2af40031cf4bb
lbodor <lazar.bodor@gmail.com>
2016-04-09 11:56:57 +1000

    Remove template comments

--- configuration.nix
+++ configuration.nix
@@ -6,30 +6,19 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda";
 
   virtualisation.virtualbox.guest.enable = true;
   boot.initrd.checkJournalingFS = false;
 
-  networking.hostName = "dev"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "dev";
 
-  # Select internationalisation properties.
-  # i18n = {
-  #   consoleFont = "Lat2-Terminus16";
-  #   consoleKeyMap = "us";
-  #   defaultLocale = "en_US.UTF-8";
-  # };
-
-  # Set your time zone.
   time.timeZone = "Australia/Sydney";
 
   nix = {
@@ -55,8 +44,6 @@
       };
   };
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
      file
      wget
@@ -95,17 +82,11 @@
        ];
    };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   services.locate.enable = true;
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
@@ -120,16 +101,6 @@
         host all all 127.0.0.1/32 md5
       '';
 
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.kdm.enable = true;
-  # services.xserver.desktopManager.kde4.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  # };
-
   users.extraUsers.lazar = {
     isNormalUser = true;
     home = "/home/lazar";
@@ -140,5 +111,4 @@
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "15.09";
-
 }
