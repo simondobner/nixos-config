d05278d16ee2c5e52b1e2b77885222c2b5279552
Lazar Bodor <lazar.bodor@gmail.com>
2016-06-20 15:01:25 +1000

    GA-vbox, add hardware configuration

--- configuration-ga-vbox.nix
+++ configuration-ga-vbox.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware-configuration-ga-vbox.nix
     ];
 
   # Use the GRUB 2 boot loader.
