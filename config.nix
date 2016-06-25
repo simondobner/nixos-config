57886d850dcc5046438c9927796e16f52e248d12
Lazar Bodor <lazar.bodor@gmail.com>
2016-06-25 20:31:28 +1000

    Add eclipse mars and neon to package overrides

--- config.nix
+++ config.nix
@@ -12,6 +12,28 @@
 
     squirrelsql = super.callPackage ./pkgs/squirrelsql {};
 
+    eclipse-ee-452 = super.eclipses.buildEclipse {
+      name = "eclipse-ee-4.5.2";
+      description = "Eclipse EE IDE";
+      sources = {
+        "x86_64-linux" = super.fetchurl {
+          url = http://download.eclipse.org/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz;
+          sha256 = "0fp2933qs9c7drz98imzis9knyyyi7r8chhvg6zxr7975c6lcmai";
+        };
+      };
+    };
+    eclipse-ee-46 = super.eclipses.buildEclipse {
+      name = "eclipse-ee-4.6";
+      description = "Eclipse EE IDE";
+      sources = {
+        "x86_64-linux" = super.fetchurl {
+          url = https://eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/R/eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz&r=1;
+          sha256 = "1wdq02gswli3wm8j1rlzk4c8d0vpb6qgl8mw31mwn2cvx6wy55rs";
+          name = "eclipse-jee-neon-R-linux-gtk-x86_64.tar.gz";
+        };
+      };
+    };
+
     systemToolsEnv = with super; buildEnv {
       name = "systemToolsEnv";
       paths = [
@@ -42,6 +64,7 @@
       paths = [
         openjdk
         maven
+        eclipse-ee-46
       ];
     };
 
